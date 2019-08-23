7b394681bf90c35a674603725c15b80626430e29
marivit <virgivitali@gmail.com>
2019-08-23 17:30:55 -0400

    comit inicial

---
+++ frontend/src/app/services/api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  API_URL = 'http://localhost:8081/api/';
+
+  constructor(private http: HttpClient) {
+  }
+
+  // private static _handleError(err: HttpErrorResponse | any) {
+  //   return Observable.throw(err.message || 'Error: Unable to complete request.');
+  // }
+
+  // // GET list of public, future events
+  // getExams(): Observable<Exam[]> {
+  //   return this.http
+  //     .get(`${this.API_URL}/exams`)
+  //     .catch(ApiService._handleError);
+  // }
+
+  public trainModel(svcParameters: SVCParameters): Observable<SVCResult> {
+    return this.http.post(`${this.API_URL}train`, svcParameters).map((res) => res.json());
+}
+}
