007efdee1f376664396588aea983ac8114a26b0b
Germano <geer1997@gmail.com>
2019-08-24 21:17:22 -0400

    Funcionamiento del predict

--- backend/server.py
+++ backend/server.py
@@ -15,6 +15,10 @@ from keras.models import Model
 # Para almacenar y obtener el modelo para predicciones futuras de un archivo .pkl.
 from sklearn.externals import joblib
 
+import os.path
+
+import sys
+
 # declare constants
 HOST = '0.0.0.0'
 PORT = 8081
@@ -25,14 +29,28 @@ api = Api(app)
 
 CORS(app)
 
+@app.route("/")
+def hello():
+    return jsonify({'text':'Hello World!'})
+
 @app.route('/api/predict',methods=['POST']) #http://localhost:8081/api/predict
 def predict():
-    model = joblib.load(open('..datos/modelos/model2.pkl','rb'))
-    data = request.get_json(force=True)
+    dataTestPath = os.path.abspath('..\\recomendation-system\\datos\modelos\\array_data_df.npy')
+    targetTestPath = os.path.abspath('..\\recomendation-system\\datos\modelos\\array_target_df.npy')
+    array_data_test = np.load(dataTestPath)
+    array_target_test = np.load(targetTestPath)
+
+    print("array", array_data_test.shape, array_target_test.shape, file=sys.stderr)
+
+    modelPath = os.path.abspath('..\\recomendation-system\\datos\modelos\\model2.pkl')
+    model = joblib.load(open(modelPath,'rb'))
+    print('model', file=sys.stderr)
+    print('modellll',model.summary(), file=sys.stderr)
+    # data = request.get_json(force=True)
     output = model.predict([array_data_test, array_target_test])
     # output = prediction[0]
-    print(output)
-    return jsonify(output)
+    print(output, file=sys.stderr)
+    return jsonify(output.tolist())
 
 if __name__ == '__main__':
     # run web server
