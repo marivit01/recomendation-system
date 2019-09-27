c79458c3b519c89034a12a61f5567ef11c929370
Germano <geer1997@gmail.com>
2019-09-26 23:40:53 -0400

    Inicial de modelo 5

--- backend/adapty.py
+++ backend/adapty.py
@@ -205,3 +205,34 @@ def getContextAndTarget(array_target, array_data):
 
   return np.asarray(new_array_historial), np.asarray(new_array_target_mat), np.asarray(new_array_target_context)
 
+
+def adaptYModel5(assigns_trim_target):
+  array_target = []
+
+  all_assigns = ['FBTCE03','FBTMM00','FBTHU01','FBTIE02','BPTQI21','BPTMI04','FBPIN01'
+  ,'BPTPI07','FBPLI02','FBTIN04','FGE0000','FBPCE04','FBPMM02','FBTIN05'
+  ,'FBPIN03','FBPIN02','FBPLI01','FBPCE03','FBPMM01','FBTHU02','FBTSP03'
+  ,'BPTFI02','BPTMI11','BPTSP05','BPTMI01','FBTCE04','FBTMM01','FGS0000'
+  ,'FBTIE03','BPTFI03','BPTMI20','BPTFI01','BPTQI22','BPTMI05','BPTMI30'
+  ,'BPTSP06','BPTMI02','BPTMI03','FPTCS16','FPTSP15','BPTEN12','BPTMI31'
+  ,'FPTEN23','BPTSP03','BPTFI04','FPTSP14','BPTDI01-1','FBTIE01','FPTSP20'
+  ,'FPTMI21','BPTSP04','FPTSP01','FPTSP18','FPTSP22','FPTSP17','FPTPI09'
+  ,'FPTSP11','FPTSP04','FPTSP02','BPTDI01-2','FPTSP23','FPTSP19','FPTSP07'
+  ,'FPTSP25','FPTSP21','FPTIS01']
+
+  all_assigns.sort()
+
+  print('LEEEEEEEEEENGTH', all_assigns.__len__())
+
+  only_assigns = {}
+  for assign_zero in all_assigns:
+    only_assigns[assign_zero] = 0
+    
+  for assign in assigns_trim_target:
+    print(assign)
+    only_assigns[assign] = 1
+    
+  array_target.append(np.array( tuple(only_assigns.values()) ))
+  print('SHAAAAAAAPE', np.asarray(array_target).shape)
+  return np.asarray(array_target)
+
