2a561a25463fff18a4f5e48c7aace55470ff9650
Germano <geer1997@gmail.com>
2019-09-29 13:36:17 -0400

    prueba modelo 5 con modelo 2 (Germano)

--- backend/server.py
+++ backend/server.py
@@ -159,7 +159,7 @@ def predictModel5(studentId):
 
     print("target result:", array_target_test, file=sys.stderr)
 
-    modelPath = os.path.abspath('..\\datos\modelos\model-5\\model5.pkl')
+    modelPath = os.path.abspath('..\\datos\modelos\\model2_5.pkl')
     model = joblib.load(open(modelPath,'rb'))
 
     output = model.predict([array_data_test, array_target_test])
